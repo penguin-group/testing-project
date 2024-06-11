1668cc3d32f440333cf0e9ce35c444f275c9b354
PyCeciliaReyes <cecibreyes@gmail.com>
2024-06-11 15:51:17 -0400

    feat: Add cost center and percentage fields to PurchaseRequisition model and view
    The code changes add the `cost_center_id` and `percentage` fields to the `PurchaseRequisition` model and the corresponding view in the XML file. This allows users to specify a cost center and percentage for each purchase requisition.

--- penguin_purchase/models/purchase_requisition.py
+++ penguin_purchase/models/purchase_requisition.py
@@ -4,4 +4,7 @@ class PurchaseRequisition(models.Model):
     _inherit = 'purchase.requisition'
 
     analytic_account_ids = fields.Many2many('account.analytic.account', string='Analytic Accounts')
+
+    cost_center_id = fields.Many2one('analytic_account_ids', string='Cost Center')
+    percentage = fields.Float(string='Percentage', required=True)
     selection_notes = fields.Char(string="Comments", required=False, )
