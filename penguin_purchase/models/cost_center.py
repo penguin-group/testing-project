41c7fd6e879c396428486bc80982873d6e82e9ef
Jhonny Salvatierra R <jhonsra@gmail.com>
2024-06-17 07:10:54 -0400

    Procurement Workflow - inicio de desarrollo de requerimientos

--- penguin_purchase/models/cost_center.py
+++ penguin_purchase/models/cost_center.py
@@ -2,6 +2,9 @@ from odoo import models, fields, api
 
 class CostCenter(models.Model):
     _name = 'cost.center'
+    _description = 'Cost Center'
+    _inherit = 'analytic.mixin'
 
-    cost_center_id = fields.Many2one('account.analytic.account', string='Cost Center')
-    percentage = fields.Float(string='Percentage', required=True)
+    #cost_center_id = fields.Many2one('account.analytic.account', string='Cost Center')
+    #percentage = fields.Float(string='Percentage', required=True)
+    name = fields.Char(string='Cost Center Name', required=True)
--- penguin_purchase/models/purchase_requisition.py
+++ penguin_purchase/models/purchase_requisition.py
@@ -14,4 +14,41 @@ class PurchaseRequisition(models.Model):
     signature4 = fields.Binary(string="Signature 4", required=False, ) #firma adicional de aprob
     signature5 = fields.Binary(string="Signature 5", required=False, ) #firma adicional de aprob
 
-    process_followed = fields.Boolean(string="Process was followed", required=False, )
+    process_followed = fields.Boolean(string="Process was followed", required=False, compute='_compute_process_followed', store=True )
+    number_of_signatories = fields.Char(string="Number of Signatories", compute='_compute_number_of_signatories',
+                                        store=True)
+
+    @api.model
+    def create(self, vals):
+        res = super(PurchaseRequisition, self).create(vals)
+        if 'line_ids' in vals:
+            for line in vals['line_ids']:
+                line[2].update({
+                    'product_classification': 'materials',  # Default classification
+                })
+        return res
+
+    @api.depends('signature1', 'signature2', 'signature3')
+    def _compute_process_followed(self):
+        for record in self:
+            record.process_followed = bool(record.signature1 or record.signature2 or record.signature3)
+
+    @api.depends('signature1', 'signature2', 'signature3')
+    def _compute_number_of_signatories(self):
+        for record in self:
+            signatories = sum(1 for sig in [record.signature1, record.signature2, record.signature3] if sig)
+            record.number_of_signatories = str(signatories)
+
+
+class PurchaseRequisitionLine(models.Model):
+    _inherit = 'purchase.requisition.line'
+
+    product_classification = fields.Selection(
+        [('labor', 'Labor'), ('materials', 'Materials'), ('units', 'Units')],
+        string='Product Classification',
+        required=True,
+    )
+    document_ids = fields.Many2many('ir.attachment', string='Documents')
+
+
+
