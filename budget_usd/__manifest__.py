9ea0272bf2e8811f7a5369ad9f04580bbfd05541
Joe <onnetpy@gmail.com>
2024-07-16 11:43:18 -0400

    [FIX] budget_usd: made installable

--- budget_usd/__manifest__.py
+++ budget_usd/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '17.0.1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','moneda_secundaria'],
+    'depends': ['base','moneda_secundaria', 'account_budget'],
 
     # always loaded
     'data': [
