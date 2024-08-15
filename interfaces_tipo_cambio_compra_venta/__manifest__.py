d5e348dee114db6b7404ab4271df131eed033620
Joe <onnetpy@gmail.com>
2024-08-15 08:55:45 -0400

    [FIX] interfaces_tipo_cambio_compra_venta: version updated

--- interfaces_tipo_cambio_compra_venta/__manifest__.py
+++ interfaces_tipo_cambio_compra_venta/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Account',
-    'version': '17.0.1.0',
+    'version': '17.0.1.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'account_accountant', 'sale'],
