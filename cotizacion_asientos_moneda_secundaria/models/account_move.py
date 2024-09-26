4464024456a1bd0f1d4968244ec3b3d83de2ce00
William Eckerleben <wgre2000@gmail.com>
2024-09-26 15:30:49 -0400

    hotfix(op-203): Update currency exchange logic in account move models
    - Replaced  with  for currency calculations.
    - Removed precision rounding in , , and  calculations.
    - Adjusted validation logic to ensure correct exchange rate handling.
    
    This hotfix addresses calculation inaccuracies due to incorrect exchange rate usage in both the  and  modules.

--- cotizacion_asientos_moneda_secundaria/models/account_move.py
+++ cotizacion_asientos_moneda_secundaria/models/account_move.py
@@ -20,13 +20,13 @@ class AccountMoveLine(models.Model):
                 tipo_cambio=self.move_id.currency_rate
             else:
                 tipo_cambio = secondary_currency_id.rate_ids.filtered(lambda x: x.name <= self.date)
-                tipo_cambio = round(tipo_cambio[0].inverse_company_rate,2)
+                tipo_cambio = round(tipo_cambio[0].rate,2)
             if not tipo_cambio:
                 raise exceptions.ValidationError('No existe un tipo de cambio definido para la fecha %s'%self.date)
         if tipo_cambio>0:
-            debit_ms = round(self.debit / tipo_cambio,2)
-            credit_ms = round(self.credit / tipo_cambio,2)
-            balance_ms = round(self.balance / tipo_cambio,2)
+            debit_ms = round(self.debit / tipo_cambio)
+            credit_ms = round(self.credit / tipo_cambio)
+            balance_ms = round(self.balance / tipo_cambio)
         else:
             debit_ms = 0
             credit_ms = 0
