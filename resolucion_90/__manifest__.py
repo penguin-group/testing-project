45a13a1ff3f666f297bb8d3ba02bd751e7277354
Joe <onnetpy@gmail.com>
2024-10-07 18:17:23 -0300

    fix(resolucion90): amount conversion in RG90 report

--- resolucion_90/__manifest__.py
+++ resolucion_90/__manifest__.py
@@ -21,7 +21,7 @@
     'version': '17.0.1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'proveedores_timbrado'],
+    'depends': ['base', 'proveedores_timbrado', 'cotizacion_asientos_moneda_secundaria'],
 
     # always loaded
     'data': [
