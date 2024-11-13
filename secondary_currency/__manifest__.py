5f4e891cecab8d913ce66f0edea2b9e6743fc11c
Joe <onnetpy@gmail.com>
2024-11-13 15:24:29 -0300

    feat(secondary_currency): The new module secondary_currency was created.

---
+++ secondary_currency/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Secondary Currency",
+
+    'summary': "Manages dual currencies for accurate transaction recording and reporting.",
+
+    'description': """
+        This module handles secondary currency so that transactions can be accurately recorded and reported 
+        in dual currencies, facilitating international transactions and financial reporting.
+    """,
+
+    'author': "Penguin Infrastructure, José González",
+    'website': "https://www.pengin.digital",
+
+    'category': 'Accounting',
+    'version': '17.0.0.1',
+
+    'depends': ['base', 'account'],
+
+    'data': [
+    ],
+    'demo': [
+    ],
+}
+
