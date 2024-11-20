9e1252d871f234c6bf174d4cf6c9cde51b148717
José González <onnetpy@gmail.com>
2024-11-20 16:16:29 -0300

    fix(secondary_currency): adjustments in secondary currency rate computations. The secondary currency is not required anymore.

--- secondary_currency/models/account_move_line.py
+++ secondary_currency/models/account_move_line.py
@@ -21,24 +21,30 @@ class AccountMoveLine(models.Model):
     @api.depends('currency_id', 'company_id', 'move_id.invoice_currency_rate', 'move_id.date')
     def _compute_secondary_currency_rate(self):
         for line in self:
-            if line.move_id.is_invoice(include_receipts=True):
-                line.secondary_currency_rate = line.move_id.invoice_secondary_currency_rate
-            elif line.currency_id:
-                line.secondary_currency_rate = self.env['res.currency']._get_conversion_rate(
-                    from_currency=line.company_secondary_currency_id,
-                    to_currency=line.company_currency_id,
-                    company=line.company_id,
-                    date=line._get_rate_date(),
-                )
+            if line.company_secondary_currency_id:
+                if line.move_id.is_invoice(include_receipts=True):
+                    line.secondary_currency_rate = line.move_id.invoice_secondary_currency_rate
+                elif line.currency_id:
+                    line.secondary_currency_rate = self.env['res.currency']._get_conversion_rate(
+                        from_currency=line.company_secondary_currency_id,
+                        to_currency=line.company_currency_id,
+                        company=line.company_id,
+                        date=line._get_rate_date(),
+                    )
+                else:
+                    line.secondary_currency_rate = 1
             else:
-                line.secondary_currency_rate = 1
+                line.secondary_currency_rate = 0
 
     @api.depends('balance')
     def _compute_secondary_balance(self):
         for line in self:
-            if line.display_type in ('line_section', 'line_note'):
-                line.secondary_balance = False
-            elif line.currency_id == line.company_secondary_currency_id:
-                line.secondary_balance = line.amount_currency
+            if line.company_secondary_currency_id:
+                if line.display_type in ('line_section', 'line_note'):
+                    line.secondary_balance = False
+                elif line.currency_id == line.company_secondary_currency_id:
+                    line.secondary_balance = line.amount_currency
+                else:
+                    line.secondary_balance = line.balance / line.secondary_currency_rate
             else:
-                line.secondary_balance = line.balance / line.secondary_currency_rate
+                line.secondary_balance = 0
