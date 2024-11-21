1043920239dad044a1c30e8e9de3359c3710a705
José González <onnetpy@gmail.com>
2024-11-21 16:29:24 -0300

    fix(l10n_py): adjustments in py reports computations

--- pisa_account/models/account_move_line.py
+++ pisa_account/models/account_move_line.py
@@ -28,27 +28,3 @@ class AccountMoveLine(models.Model):
                         line.secondary_balance = line.company_secondary_currency_id.round(line.balance / line.secondary_currency_rate)
             else:
                 line.secondary_balance = 0
-
-    def _compute_fields_for_py_reports(self):
-        if self.env.company.sec_currency_id and self.env.company.sec_currency_id.name == 'PYG' and self.env.company.country_code == 'PY':
-            for record in self:
-                record.amount_base10 = record.amount_vat10 = record.amount_base5 = record.amount_vat5 = record.amount_exempt = record.amount_taxable_imports = 0
-                
-                if any([tax.amount == 0 for tax in record.tax_ids]):
-                    record.amount_exempt = abs(record.secondary_balance)
-                elif record.tax_line_id and record.tax_line_id.amount == 10:
-                    record.amount_base10 = record.tax_base_amount
-                    record.amount_vat10 = abs(record.secondary_balance)
-                elif record.tax_line_id and record.tax_line_id.amount == 5:
-                    record.amount_base5 = record.tax_base_amount
-                    record.amount_vat5 = abs(record.secondary_balance)
-
-                # Handle import clearance invoices
-                if record.move_id.move_type == "out_invoice" and record.move_id.import_clearance:
-                    if record.account_id.vat_import:
-                        record.amount_vat10 = exempt
-                        record.amount_base10 = record.amount_vat10 * 10
-                        record.amount_taxable_imports = record.amount_base10
-                    exempt = 0
-        else:
-            super(AccountMoveLine, self)._compute_fields_for_py_reports()
