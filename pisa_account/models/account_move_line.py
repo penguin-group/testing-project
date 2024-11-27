44f75056d9fcc92af3989067bce71738faa104f0
William Eckerleben <wgre2000@gmail.com>
2024-11-27 11:04:10 -0300

    feat(op-287): Eliminate rounding for secondary balance in specific currency context
    - Removed overridden  method in .
    - Refactored balance calculations to remove rounding for currencies like PYG where specific adjustments were previously applied.

--- pisa_account/models/account_move_line.py
+++ pisa_account/models/account_move_line.py
@@ -7,25 +7,4 @@ class AccountMoveLine(models.Model):
     def _compute_secondary_currency_rate(self):
         for line in self:
             line.secondary_currency_rate = line.move_id.invoice_secondary_currency_rate
-
-    @api.depends('balance')
-    def _compute_secondary_balance(self):
-        # Override the original method from the secondary_currency module to modify the rounding, 
-        # which should be 0 decimal places, although the rounding field in the currency settings is set to two decimal places.
-        for line in self:
-            line._compute_company_secondary_currency_id()
-            if line.company_secondary_currency_id:
-                if line.display_type in ('line_section', 'line_note'):
-                    line.secondary_balance = False
-                elif line.currency_id == line.company_secondary_currency_id:
-                    if line.company_secondary_currency_id.name == "PYG":
-                        line.secondary_balance = tools.float_round(line.amount_currency, 0)
-                    else:
-                        line.secondary_balance = line.company_secondary_currency_id.round(line.amount_currency)
-                else:
-                    if line.company_secondary_currency_id.name == "PYG":
-                        line.secondary_balance = tools.float_round(line.balance / line.secondary_currency_rate, 0)
-                    else:
-                        line.secondary_balance = line.company_secondary_currency_id.round(line.balance / line.secondary_currency_rate)
-            else:
-                line.secondary_balance = 0
+    
