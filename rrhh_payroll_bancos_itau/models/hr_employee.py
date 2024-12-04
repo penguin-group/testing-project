90d3dc13fed6f6508bb81808bc3c4fa90f5ac678
William Eckerleben <wgre2000@gmail.com>
2024-12-04 14:31:30 -0300

    chore(cleanup): Removed legacy payroll and payslip modules

--- rrhh_payroll_bancos_itau/models/hr_employee.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-
-
-class HREmployeeBase(models.AbstractModel):
-    _inherit = 'hr.employee.base'
-
-    banco_itau_nro_cuenta = fields.Char(string='Nro Cuenta ITAU')
