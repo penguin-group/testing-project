9bc3ca2fdf4b6b3cf440c226d535cc5ce64988ba
William Eckerleben <wgre2000@gmail.com>
2024-12-10 19:20:07 -0300

    chore(cleanup-legacy-modules): Removed outdated modules and their associated files
    - Deleted unused modules and files including:
      - , , , , .
      - Static assets, model definitions, views, security rules, reports, and related configurations.
    - Cleared redundant dependencies and legacy implementations no longer needed in the project.
    
    These changes aim to declutter the repository and streamline future development.

--- rrhh_liquidacion/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import res_company, res_config_settings, hr_payslip, wizard_calculo_despido, wizard_calculo_renuncia, wizard_calculo_jubilacion, \
-    hr_payroll_structure_type
