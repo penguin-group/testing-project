4ecd865883440528c3104de6e34f6710b0b999f9
Joe <onnetpy@gmail.com>
2024-12-16 14:05:06 -0300

    rrhh_liquidacion module deleted

--- rrhh_liquidacion/models/hr_payroll_structure_type.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models, api
-
-
-class HrPayrollStructureType(models.Model):
-    _inherit = 'hr.payroll.structure.type'
-
-    structure_type_tag = fields.Selection(selection_add=[
-        ('liquidacion', 'Liquidación'),
-    ], ondelete={'liquidacion': 'set default'})
