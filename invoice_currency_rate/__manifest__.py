6248e48f4235fa29b566558a78158a3888b21637
Joe <onnetpy@gmail.com>
2024-12-16 15:41:37 -0300

    feat(invoice_currency_rate): Added module to enable uninstallation

---
+++ invoice_currency_rate/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Invoice Currency Rate",
+
+    'summary': "This module allows to set currency rate for invoices",
+
+    'description': """
+        This module allows to set currency rate for invoices
+    """,
+
+    'author': "Penguin Infrastructure S.A., José González",
+    'website': "https://penguin.digital",
+
+    'category': 'Accounting',
+    'version': '18.0.0.1',
+    'license': "OPL-1",
+
+    'depends': ['base', 'account'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/account_move_views.xml',
+    ],
+
+}
+
