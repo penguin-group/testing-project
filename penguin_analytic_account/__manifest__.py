1e74d741ed1412094a1da8ff94219b22e2ddc4fe
Ivanrcaceres <ivancaceres17@gmail.com>
2025-01-15 19:45:22 -0300

    feat/op-334-make-analytic-accounts-required-for-bills--invoices--payments--and-journal-entries, fix

--- penguin_analytic_account/__manifest__.py
+++ penguin_analytic_account/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '18.0.1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','account','analytic'],
+    'depends': ['base','account','analytic', 'sale', 'purchase'],
 
     # always loaded
     'data': [
