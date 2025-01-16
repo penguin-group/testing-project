bab75063eee788b30a389e34ee4637a68651a187
Joe <onnetpy@gmail.com>
2025-01-16 06:50:55 -0300

    feat(pisa_account_fix): add methods to fix incorrect amount fields and enhance reconciliation process

--- pisa_account_fix/models/account_move.py
+++ pisa_account_fix/models/account_move.py
@@ -6,62 +6,24 @@ _logger = logging.getLogger(__name__)
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    def update_signed_amounts(self):
-        """
-        Update the signed amounts for the moves that have the same currency as the company currency.
-        """
-        moves = self.env['account.move'].search([('state', '=', 'posted')])
-        # Filter moves that have the same currency as the company currency and have different signed amounts
-        moves = moves.filtered(lambda m: 
-            m.currency_id.id == m.company_id.currency_id.id 
-            and (
-                abs(m.amount_untaxed) != abs(m.amount_untaxed_signed) or
-                abs(m.amount_tax) != abs(m.amount_tax_signed) or 
-                abs(m.amount_total) != abs(m.amount_total_signed) or 
-                abs(m.amount_residual) != abs(m.amount_residual_signed)
-            )
-        )
-        cnt = 0
-        len_moves = len(moves)
-        for move in moves:
-            cnt += 1
-            percentage = (cnt / len_moves) * 100
-            _logger.info('Processing move %s (%s/%s - %.2f%%)' % (move.name, cnt, len_moves, percentage))
-            if move.amount_untaxed_signed >= 0:
-                move.amount_untaxed_signed = abs(move.amount_untaxed)
-            else:
-                move.amount_untaxed_signed = -abs(move.amount_untaxed)
-            
-            if move.amount_tax_signed >= 0:
-                move.amount_tax_signed = abs(move.amount_tax)
-            else:
-                move.amount_tax_signed = -abs(move.amount_tax)
-            
-            if move.amount_total_signed >= 0:
-                move.amount_total_signed = abs(move.amount_total)
-            else:
-                move.amount_total_signed = -abs(move.amount_total)
-            
-            if move.amount_residual_signed >= 0:
-                move.amount_residual_signed = abs(move.amount_residual)
-            else:
-                move.amount_residual_signed = -abs(move.amount_residual)
-
+    reconciliation_fixed = fields.Boolean(string='Reconciliation Fixed', default=False)
+    
     def recompute_currency_rate(self):
         """
         Recompute the currency rate for the moves that have the invoice currency rate set to 1.
         """
         query = """
             UPDATE account_move 
-            SET invoice_currency_rate = (
-                SELECT rate 
-                FROM res_currency_rate 
-                WHERE currency_id = %s 
-                AND company_id = %s 
-                AND name <= account_move.date 
-                ORDER BY name DESC 
-                LIMIT 1
-            )
+            SET 
+                invoice_currency_rate = (
+                    SELECT rate 
+                    FROM res_currency_rate 
+                    WHERE currency_id = %s 
+                    AND company_id = %s 
+                    AND name <= account_move.date 
+                    ORDER BY name DESC 
+                    LIMIT 1
+                )
             WHERE state = 'posted' 
             AND company_id = %s 
             AND currency_id = %s 
@@ -82,16 +44,17 @@ class AccountMove(models.Model):
         """
         self.ensure_one()
         partials = []
-        for partial in self.invoice_payments_widget['content']:
-            partial_id = self.env['account.partial.reconcile'].browse(partial.get('partial_id'))
-            if partial_id.exists():
-                if not partial['is_exchange'] and partial_id.debit_move_id:
-                    partials.append({
-                        'id': partial_id.id,
-                        'line_id': partial_id.debit_move_id.id if partial_id.debit_move_id else False,
-                    })
-                if remove_partials:
-                    self.js_remove_outstanding_partial(partial_id.id)
+        if self.invoice_payments_widget:
+            for partial in self.invoice_payments_widget['content']:
+                partial_id = self.env['account.partial.reconcile'].browse(partial.get('partial_id'))
+                if partial_id.exists():
+                    if not partial['is_exchange'] and partial_id.debit_move_id:
+                        partials.append({
+                            'id': partial_id.id,
+                            'line_id': partial_id.debit_move_id.id if partial_id.debit_move_id else False,
+                        })
+                    if remove_partials:
+                        self.js_remove_outstanding_partial(partial_id.id)
         return partials
 
     def _fix_reconciliation(self):
@@ -108,4 +71,99 @@ class AccountMove(models.Model):
                     for partial in partials:
                         # Reconcile
                         if move.amount_residual > 0:
+                            move.reset_me()
+                            move.reset_payment_entry(partial['line_id'])
                             move.js_assign_outstanding_line(partial['line_id'])
+                            _logger.info('Reconciliation fixed for move %s' % move.name)
+                move.reconciliation_fixed = True
+
+    def _fix_incorrect_amount_fields(self):
+        """
+        Fix the incorrect amount fields for account moves and account move lines.
+        """
+
+        # account_move
+        query = """
+            UPDATE account_move 
+            SET
+            amount_untaxed_signed = amount_untaxed,
+            amount_tax_signed = amount_tax,
+            amount_total_signed = amount_total,
+            amount_residual_signed = amount_residual
+            WHERE 
+            currency_id = 2
+            AND company_id = 1
+            AND (
+                abs(amount_untaxed_signed) != abs(amount_untaxed)
+                OR abs(amount_tax_signed) != abs(amount_tax)
+                OR abs(amount_total_signed) != abs(amount_total) 
+                OR abs(amount_residual_signed) != abs(amount_residual)
+            )
+        """
+        try:
+            self.env.cr.execute(query)
+            self.env.cr.commit()
+            _logger.info('Incorrect amount fields fixed for account moves')
+        except Exception as e:
+            self.env.cr.rollback()
+            _logger.error('Error fixing incorrect amount fields for account moves: %s', e)
+
+        # account_move_line
+        query = """
+            WITH calculated_base AS (
+                SELECT
+                    aml.id AS aml_id,
+                    SUM(ABS(sub_aml.balance)) AS new_tax_base_amount
+                FROM 
+                    account_move_line aml
+                JOIN 
+                    account_move_line sub_aml
+                    ON sub_aml.move_id = aml.move_id
+                JOIN 
+                    account_move_line_account_tax_rel amlatr 
+                    ON amlatr.account_move_line_id = sub_aml.id
+                JOIN 
+                    account_tax tax 
+                    ON tax.id = amlatr.account_tax_id
+                WHERE
+                    tax.amount != 0
+                    AND tax.id = aml.tax_line_id
+                GROUP BY aml.id
+            )
+            UPDATE 
+                account_move_line aml
+            SET 
+                tax_base_amount = cb.new_tax_base_amount
+            FROM 
+                calculated_base cb
+            WHERE 
+                aml.id = cb.aml_id
+                AND aml.company_id = 1
+                AND aml.tax_base_amount != cb.new_tax_base_amount
+        """
+        try:
+            self.env.cr.execute(query)
+            self.env.cr.commit()
+            _logger.info('Tax base amount updated successfully for account move lines')
+        except Exception as e:
+            self.env.cr.rollback()
+            _logger.error('Error updating tax base amount for account move lines: %s', e)
+
+    def reset_payment_entry(self, line_id):
+        """
+        Reset the payment entry for the invoice.
+        """
+        line_id = self.env['account.move.line'].browse(line_id)
+        line_id.move_id.reset_me()
+
+    def reset_me(self):
+        """
+        Reset the moves.
+        """
+        self.ensure_one()
+        try:
+            self.button_draft()
+            self.action_post()
+            _logger.info('Move %s reset successfully', self.name)
+        except Exception as e:
+            _logger.error('Error resetting move %s: %s', self.name, e)
