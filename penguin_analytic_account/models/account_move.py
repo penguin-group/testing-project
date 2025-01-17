1cb4a7bb2cef4300bdcfaa726eb22ac983bb8f50
Ivanrcaceres <ivancaceres17@gmail.com>
2025-01-17 08:41:42 -0300

    feat/op-334

--- penguin_analytic_account/models/account_move.py
+++ penguin_analytic_account/models/account_move.py
@@ -5,44 +5,37 @@ from odoo.tools.float_utils import float_is_zero
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    @api.model
-    def create(self, vals):
-        if self.move_type == 'out_invoice' or self.move_type == 'in_invoice':
-            if not vals.get('analytic_distribution'):
-                raise ValidationError("You must provide a value for Analytic Distribution on the lines.")
-            return super(AccountMoveLine, self).create(vals)
-
-    def write(self, vals):
-        if self.move_type == 'out_invoice' or self.move_type == 'in_invoice':
-            if 'analytic_distribution' not in vals and not self.analytic_distribution:
-                raise ValidationError("You must provide a value for Analytic Distribution on the lines.")
-            return super(AccountMoveLine, self).write(vals)
+    @api.constrains('analytic_distribution')
+    def _check_analytic_distribution(self):
+        for line in self:
+            if line.move_id.is_invoice(include_receipts=True):
+                if line.product_id:
+                    if not line.analytic_distribution:
+                        raise ValidationError(
+                            "The analytic distribution is mandatory for all invoice lines."
+                        )
 
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    @api.model
-    def create(self, vals):
-        if not vals.get('analytic_distribution'):
-            raise ValidationError("You must provide a value for Analytic Distribution on the sales lines.")
-        return super(SaleOrderLine, self).create(vals)
-
-    def write(self, vals):
-        if 'analytic_distribution' not in vals and not self.analytic_distribution:
-            raise ValidationError("You must provide a value for Analytic Distribution on the sales lines.")
-        return super(SaleOrderLine, self).write(vals)
+    @api.constrains('analytic_distribution')
+    def _check_analytic_distribution(self):
+        for line in self:
+                if line.product_id:
+                    if not line.analytic_distribution:
+                        raise ValidationError(
+                            "The analytical distribution is mandatory for all lines."
+                        )
 
 class SaleOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    @api.model
-    def create(self, vals):
-        if not vals.get('analytic_distribution'):
-            raise ValidationError("You must provide a value for Analytic Distribution on the purchase lines.")
-        return super(SaleOrderLine, self).create(vals)
-
-    def write(self, vals):
-        if 'analytic_distribution' not in vals and not self.analytic_distribution:
-            raise ValidationError("You must provide a value for Analytic Distribution on the purchase lines.")
-        return super(SaleOrderLine, self).write(vals)
+    @api.constrains('analytic_distribution')
+    def _check_analytic_distribution(self):
+        for line in self:
+                if line.product_id:
+                    if not line.analytic_distribution:
+                        raise ValidationError(
+                            "The analytical distribution is mandatory for all lines."
+                        )
