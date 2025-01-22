5d00761663fd7f209b98a80b84e87d8982831eeb
Joe <onnetpy@gmail.com>
2025-01-22 16:26:39 -0300

    feat(temp_fix_op-345): create temporary module to reset journal entries and fix secondary balance errors

---
+++ temp_fix_op-345/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Fix OP-345",
+
+    'summary': "Temporary module for OP-345",
+
+    'description': """
+        Temporary module for reset journal entry 015-001-003045 and related payments to draft and fix secondary balance errors
+    """,
+
+    'author': "Penguin Infrastructure, José González",
+    'website': "https://penguin.digital",
+
+    'category': 'Accounting',
+    'version': '18.0.1.0',
+
+    'depends': ['secondary_currency'],
+
+    'data': [
+    ],
+}
+
