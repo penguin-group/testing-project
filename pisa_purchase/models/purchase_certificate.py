de601e5f9864332c778a99365c4c075ad408a15e
Joe <onnetpy@gmail.com>
2025-02-24 13:38:32 -0300

    feat(pisa_purchase): enhance PurchaseCertificate model with abstract inheritance and notification on certificate file updates

--- pisa_purchase/models/purchase_certificate.py
+++ pisa_purchase/models/purchase_certificate.py
@@ -1,12 +1,12 @@
 from odoo import models, fields, api, _
 
-class PurchaseCertificate(models.Model):
+class PurchaseCertificate(models.Model, models.AbstractModel):
     _name = 'purchase.certificate'
     _description = 'Purchase Certificate'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
     name = fields.Char(string='Name', required=True, tracking=True)
-    certificate_file = fields.Binary(string='Certificate', attachment=True, tracking=True)
+    certificate_file = fields.Binary(string='Certificate', attachment=True)
     state = fields.Selection([
         ('draft', 'Draft'),
         ('sent', 'Sent'),
@@ -16,3 +16,8 @@ class PurchaseCertificate(models.Model):
     def action_confirm(self):
         self.ensure_one()
         self.write({'state': 'approved'})
+
+    def write(self, vals):
+        if 'certificate_file' in vals:
+            self.message_post(body="Certificate file has been updated.")
+        return super(PurchaseCertificate, self).write(vals)
