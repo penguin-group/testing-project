21300f6ac6236ae0a06f3517397ddaea9f1afe69
Joe <onnetpy@gmail.com>
2025-03-11 14:51:51 -0300

    feat(pisa_purchase): add technical review and sourcing strategy actions to purchase order, update button visibility in views

--- pisa_purchase/models/purchase_order.py
+++ pisa_purchase/models/purchase_order.py
@@ -14,3 +14,27 @@ class PurchaseOrder(models.Model):
         ('done', 'Locked'),
         ('cancel', 'Cancelled')
     ], string='Status', readonly=True, index=True, copy=False, default='draft', tracking=True)
+
+    def action_request_technical_review(self):
+        self.write({'state': 'technical_review'})
+
+    def action_request_sourcing_strategy(self):
+        self.write({'state': 'sourcing_strategy'})
+
+    def button_confirm(self):
+        """
+        Override the purchace.order original method just to change the previous state to 'sourcing_strategy'
+        """
+        for order in self:
+            if order.state not in ['sourcing_strategy']:
+                continue
+            order.order_line._validate_analytic_distribution()
+            order._add_supplier_to_product()
+            # Deal with double validation process
+            if order._approval_allowed():
+                order.button_approve()
+            else:
+                order.write({'state': 'to approve'})
+            if order.partner_id not in order.message_partner_ids:
+                order.message_subscribe([order.partner_id.id])
+        return True
