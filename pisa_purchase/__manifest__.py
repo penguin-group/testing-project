b5700c06a05101fe2cfd7108581960f2bba1e7be
Joe <onnetpy@gmail.com>
2025-03-26 19:59:49 -0300

    feat(pisa_purchase): Add fields and attachments in purchase requisition alternative creation

---
+++ pisa_purchase/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "PISA Purchase",
+
+    'summary': "Customized features for PISA in the purchase application.",
+
+    'description': """
+        Customized features for PISA in the purchase application.
+    """,
+
+    'author': "Penguin Infrastructure S.A.",
+    'mantainers': ['José González'],
+    'website': "https://penguin.digital",
+
+    'category': 'Accounting',
+    'version': '18.0.1.0.0',
+    'license': "OPL-1",
+
+    'depends': ['base', 'purchase_requisition', 'purchase_tier_validation', 'purchase_request_tier_validation', 'completion_certificates', 'project', 'account'],
+
+    'data': [
+        'views/purchase_views.xml',
+        'views/purchase_request_views.xml',
+    ],
+}
+
