edd0b19f102017a1dbb3acb8268ada97147b592c
David Paez <paezdavidr@gmail.com>
2025-06-11 16:46:43 -0300

    chore(purchase requests): hide unnecessary menuitems from all users

--- pisa_purchase/__manifest__.py
+++ pisa_purchase/__manifest__.py
@@ -13,13 +13,16 @@
     'website': "https://penguin.digital",
 
     'category': 'Accounting',
-    'version': '18.0.1.1.0',
+    'version': '18.0.1.1.1',
     'license': "OPL-1",
 
-    'depends': ['base', 'purchase_requisition', 'purchase_tier_validation', 'purchase_request_tier_validation', 'completion_certificates', 'project', 'account'],
+    'depends': ['base', 'purchase_requisition', 'purchase_tier_validation', 'purchase_request_tier_validation', 'completion_certificates', 'project', 'account', 'purchase_request'],
 
     'data': [
         'views/purchase_views.xml',
         'views/purchase_request_views.xml',
+        'views/menu_override.xml',
+        # 'security/purchase_request.xml',
+        # 'security/ir.model.access.csv'
     ],
 }
