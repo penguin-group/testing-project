b6f7b7e91342f59ff1ac9b823d3b68c1e20f970f
José González <onnetpy@gmail.com>
2025-07-14 16:43:30 -0300

    feat(pisa_project): add initial module structure with custom project management features
    - Introduced the PISA Project module to extend Odoo's Project application.
    - Added a new 'Manager' group with specific access rights and record rules.
    - Implemented security settings and access control for project managers.
    - Included README and manifest files for module documentation and metadata.
    
    This module aims to enhance project management workflows tailored to PISA requirements.

---
+++ pisa_project/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "PISA Project",
+
+    'summary': "Custom enhancements for project management in Odoo, tailored for PISA requirements.",
+
+    'description': """
+This module extends Odoo's Project application to support custom workflows and permissions for PISA projects. 
+It introduces a new 'Manager' group with specific access rights and record rules, allowing team managers to 
+fully manage their own projects and related records (such as stages, tasks, tags, milestones, collaborators, 
+and analytic accounts). 
+Project managers can only view and manage the projects they have created, ensuring data privacy and responsibility. 
+    """,
+
+    'author': "Penguin Infrastructure",
+    'maintainers': ['José González'],
+    'website': "https://penguin.digital",
+    'category': 'Project',
+    'version': '18.0.1.0.0',
+    'license': "OPL-1", 
+
+    'depends': ['base', 'project', 'hr'],
+
+    'data': [
+        'security/project_security.xml',
+        'security/ir.model.access.csv',
+    ],
+}
+
