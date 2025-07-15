1df99a183b6925ca3b604c1d5e9a3c3e95ba1563
José González <onnetpy@gmail.com>
2025-07-15 13:04:23 -0300

    chore(pisa_purchase): update version to 18.0.1.3.0 in manifest

--- pisa_purchase/__manifest__.py
+++ pisa_purchase/__manifest__.py
@@ -13,7 +13,7 @@
     'website': "https://penguin.digital",
 
     'category': 'Accounting',
-    'version': '18.0.1.2.0',
+    'version': '18.0.1.3.0',
     'license': "OPL-1",
 
     'depends': ['base', 'purchase_requisition', 'purchase_tier_validation', 'purchase_request_tier_validation', 'completion_certificates', 'project', 'account', 'purchase_request'],
