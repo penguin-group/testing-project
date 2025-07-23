7bcab6a4620dc227eb5573a59bbe9fd513ec77fb
José González <onnetpy@gmail.com>
2025-07-23 08:07:13 -0300

    fix(manifest): update version number to 18.0.1.2.1

--- pisa_purchase/__manifest__.py
+++ pisa_purchase/__manifest__.py
@@ -13,7 +13,7 @@
     'website': "https://penguin.digital",
 
     'category': 'Accounting',
-    'version': '18.0.1.2.0',
+    'version': '18.0.1.2.1',
     'license': "OPL-1",
 
     'depends': ['base', 'purchase_requisition', 'purchase_tier_validation', 'purchase_request_tier_validation', 'stock', 'completion_certificates', 'project', 'account', 'purchase_request'],
