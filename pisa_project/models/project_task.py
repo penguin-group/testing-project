0be4826b820f7a2f53d39a55d71a2aafbc88725d
José González <onnetpy@gmail.com>
2025-08-12 14:20:55 -0300

    refactor (project): move _compute_task_code method above overrides OP-776

--- pisa_project/models/project_task.py
+++ pisa_project/models/project_task.py
@@ -7,6 +7,14 @@ class ProjectTask(models.Model):
 
     task_code = fields.Char(string='Code', compute="_compute_task_code", store=True)
 
+    def _compute_task_code(self):
+        for task in self:
+            if task.project_id:
+                project_initials = ''.join([word[0].upper() for word in task.project_id.name.split() if word])
+                task.task_code = f"{project_initials}-{task.id}"
+            else:
+                task.task_code = f"{task.id}"
+
     @api.model_create_multi
     def create(self, vals_list):
         records = super().create(vals_list)
@@ -19,11 +27,3 @@ class ProjectTask(models.Model):
         if 'project_id' in vals:
             self._compute_task_code()
         return result
-
-    def _compute_task_code(self):
-        for task in self:
-            if task.project_id:
-                project_initials = ''.join([word[0].upper() for word in task.project_id.name.split() if word])
-                task.task_code = f"{project_initials}-{task.id}"
-            else:
-                task.task_code = f"{task.id}"
