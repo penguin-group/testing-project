a42acc954d1417b352ad319d9050831938e59b3b
José González <onnetpy@gmail.com>
2025-08-12 14:08:28 -0300

    feat(project): ensure task code computation on create and project change OP-776

--- pisa_project/models/project_task.py
+++ pisa_project/models/project_task.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
-from odoo import fields,models
+from odoo import fields, models, api
 
 
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
     task_code = fields.Char(string='Code', compute="_compute_task_code", store=True)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+        for record in records:
+            record._compute_task_code()
+        return records
+
+    def write(self, vals):
+        result = super().write(vals)
+        if 'project_id' in vals:
+            self._compute_task_code()
+        return result
 
     def _compute_task_code(self):
         for task in self:
