5a442723596d38cd09771913bcf3afebf1ccdd47
José González <onnetpy@gmail.com>
2025-08-12 14:35:23 -0300

    Resolve merge conflict in task_code assignment

--- pisa_project/models/project_task.py
+++ pisa_project/models/project_task.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-from odoo import fields,models,api
-
+from odoo import fields, models, api
 
 class ProjectTask(models.Model):
     _inherit = 'project.task'
-
     task_code = fields.Char(string='Code', compute="_compute_task_code", store=True)
-
+    
     def _compute_task_code(self):
         for task in self:
             if task.project_id:
@@ -14,14 +12,14 @@ class ProjectTask(models.Model):
                 task.task_code = f"{project_initials}-{task.id}"
             else:
                 task.task_code = f"{task.id}"
-
+    
     @api.model_create_multi
     def create(self, vals_list):
         records = super().create(vals_list)
         for record in records:
             record._compute_task_code()
         return records
-
+    
     def write(self, vals):
         result = super().write(vals)
         if 'project_id' in vals:
