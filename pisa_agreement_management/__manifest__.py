b8c5170056f92c948244b911bb7ffbca5e57fbd5
David Paez <paezdavidr@gmail.com>
2025-08-27 16:53:26 -0300

    feat(OP-872): kanban view sorting depends on agreement stage
    - I removed the stage field from pisa.agreement and created stage_id
    instead. If the columns of a kanban depend on a Selection field, we can't
    arrange the columns in non-alphabetical order. Having the stages
    as a related field allows for more flexibility and we can move the kanban
    columns freely.

--- pisa_agreement_management/__manifest__.py
+++ pisa_agreement_management/__manifest__.py
@@ -12,6 +12,7 @@
         "security/ir.model.access.csv",
         "views/pisa_agreement_views.xml",
         "views/agreement_type_views.xml",
+        "views/agreement_stage_views.xml",
         "views/legal_process_type_views.xml",
         "views/pisa_agreement_menu_views.xml",
     ]
--- pisa_agreement_management/models/__init__.py
+++ pisa_agreement_management/models/__init__.py
@@ -1,3 +1,4 @@
 from . import pisa_agreement
 from . import agreement_type
 from . import legal_process_type
+from . import agreement_stage
