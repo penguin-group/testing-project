0f53b8304bd918bf5ace4f643d93ed98b8d27cb3
David Paez <paezdavidr@gmail.com>
2025-09-11 15:42:45 -0300

    feat(OP-855): expand manifest

--- pisa_approvals_expenses/__manifest__.py
+++ pisa_approvals_expenses/__manifest__.py
@@ -1,7 +1,8 @@
 {
     "name": "PISA Approval Expenses",
-    "summary": "Approvals",
+    "summary": "Adds bank and currency fields to approval types and creates a bill for employee advancement.",
     "version": "18.0.1.0.0",
+    'license': "OPL-1",
     "website": "https://penguin.digital",
     'author': "Penguin Infrastructure",
     'maintainers': ['David Páez'],
