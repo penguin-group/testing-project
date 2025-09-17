4d27b5facba8862dac01df0f47de16af33fde6d8
David Paez <paezdavidr@gmail.com>
2025-09-17 11:41:41 -0300

    feat(OP-901): add confirmation wizard before cancelling PO

--- pisa_purchase/models/purchase_order.py
+++ pisa_purchase/models/purchase_order.py
@@ -97,3 +97,17 @@ class PurchaseOrder(models.Model):
                 unlinked_po.with_context(no_reciprocal=True).write({
                     'extra_cost_po_ids': [(3, self.id)]
                 })
+
+    def action_cancel_with_confirmation(self):
+        """Wizard to double-check before cancelling a PO."""
+        self.ensure_one()
+
+        return {
+            'name': _('Double-check before cancelling'),
+            'type': 'ir.actions.act_window',
+            'res_model': 'purchase.cancel.wizard',
+            'view_mode': 'form',
+            'view_id': self.env.ref('pisa_purchase.view_purchase_cancel_wizard_form').id,
+            'target': 'new',
+            'context': {'default_purchase_order_id': self.id},
+        }
