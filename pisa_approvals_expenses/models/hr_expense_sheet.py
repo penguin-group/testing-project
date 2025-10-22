7a014a22c20edee5447a13082f15ceb5f56b290d
David Paez <paezdavidr@gmail.com>
2025-10-22 14:02:00 -0300

    feat(OP-958): add outstanding balance to expense report
    - Add approval amount and outstanding balance to the report view
    - Calculate outstanding balance depending on approval bill and report amounts
    - Add translation

--- pisa_approvals_expenses/models/hr_expense_sheet.py
+++ pisa_approvals_expenses/models/hr_expense_sheet.py
@@ -8,6 +8,7 @@ class HrExpenseSheet(models.Model):
     approval_id = fields.Many2one("approval.request",
                                   readonly=False,
                                   string="Approval Request")
+    approval_amount = fields.Float(related="approval_id.amount", string="Approval Amount")
 
     employee_user_id = fields.Many2one(
         'res.users',
@@ -16,9 +17,41 @@ class HrExpenseSheet(models.Model):
         store=False,
     )
 
+    outstanding_balance = fields.Monetary(string='Outstanding Balance',
+                                          compute='_compute_outstanding_balance',
+                                          currency_field='currency_id',
+                                          readonly=True,
+                                          store=True)
+
     @api.onchange("approval_id")
     def _check_approval_and_expense_currency(self):
-        print(1)
         if self.approval_id.currency_id != self.currency_id:
-            raise UserError(_("The approval request's currency (%s) doesn't match the expense report's currency (%s).",
-                              self.approval_id.currency_id.name, self.currency_id.name))
+            raise UserError(_("The approval request's currency (%s) doesn't match the expense report's currency (%s).", self.approval_id.currency_id.name, self.currency_id.name))
+
+
+    @api.depends('approval_id', 'currency_id', 'total_amount', 'total_amount_currency')
+    def _compute_outstanding_balance(self):
+        for report in self:
+            if not report.approval_id:
+                report.outstanding_balance = 0
+                continue
+
+            report.outstanding_balance = report.approval_id.related_vendor_bill.amount_total - report.total_amount_currency
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
